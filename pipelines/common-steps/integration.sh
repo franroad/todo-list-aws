27596e9810b23f35de2206a6a384aabfeddb6d92
franroad <76252009+franroad@users.noreply.github.com>
2023-06-10 17:07:08 +0200

    Update integration.sh

--- pipelines/common-steps/integration.sh
+++ pipelines/common-steps/integration.sh
@@ -2,5 +2,7 @@
 
 source todo-list-aws/bin/activate
 set -x
-export BASE_URL=$1
+#export BASE_URL=$1
+export BASE_URL= "https://i0169k5q84.execute-api.us-east-1.amazonaws.com/Prod
+pipelines/common-steps/integration.sh $BASE_URL"
 pytest -s test/integration/todoApiTest.py
