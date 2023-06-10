facd0dd5d9aa322159beaab333640b517db9349a
franroad <76252009+franroad@users.noreply.github.com>
2023-06-10 16:24:47 +0200

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,4 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-60dq2bphodjw --no-fail-on-empty-changeset --no-progressbar
